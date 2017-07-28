0775ebb4f1cdb8429c4c577502be0d195205a208
nadiaivannova <nadiaivannova@gmail.com>
2017-07-28 15:20:51 -0400

    index page skeleton and method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,12 @@ has_many :available_times
 has_many :bookings
 has_many :users, through: :bookings
 
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurants = Restaurant.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'users#new'
+root 'user#new'
   # root 'restaurant#index'
 
   resources :restaurants
-  resources :users
+  resource  :user
   resources :bookings
 
 end
